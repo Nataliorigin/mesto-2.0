75a1f70cd7fb66c3847dcdd08f41d5fbbdaf06f9
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-11-17 18:20:27 +0500

    Добавить кнопку переключателя и анимцию "снег"

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,10 @@ const profileActivityElement = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.form');
 const nameInput = document.querySelector('.form__input_name_username');
 const activityInput = document.querySelector('.form__input_name_activity');
+/*Добавление избражения*/
+const elementsContainer = document.querySelector('.elements__container');
+const addButton = document.querySelector('.profile__add-button');
+
 
 const openPopup = function (event) {
   popupElement.classList.add('popup_opened');
@@ -24,6 +28,18 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
+// function addElement() {
+//   elementsContainer.insertAdjacentHTML('afterbegin',`<li class="elements__item item">
+//               <img class="item__image" src="./img/alsu-vershinina.jpg"
+//                 alt="Украшенная елка">
+//               <div class="item__container">
+//                 <h2 class="item__title">Ёлка</h2>
+//                 <div class="item__like "></div>
+//               </div>
+//             </li>`);
+// }
+
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+// addButton.addEventListener('click', addElement);
